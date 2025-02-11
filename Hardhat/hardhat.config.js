31f743c485842124ce81f6ad9735413d5087f3cd
Muhammad Usman Saleem <60146013+usmankamboh@users.noreply.github.com>
2025-02-11 19:18:18 +0500

    Update hardhat.config.js

--- Hardhat/hardhat.config.js
+++ Hardhat/hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+// Ensure your configuration variables are set before executing the script
+const { vars } = require("hardhat/config");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.20",
+  paths: {
+    sources: "./contracts",
+  },
+  networks: {
+    sepolia: {
+      url: https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY},
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
