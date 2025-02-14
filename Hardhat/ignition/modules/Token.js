633ba6fbff9aa71140300ed1b071488623b1da0d
Muhammad Usman Saleem <60146013+usmankamboh@users.noreply.github.com>
2025-02-14 09:02:46 +0500

    Update Token.js

--- Hardhat/ignition/modules/Token.js
+++ Hardhat/ignition/modules/Token.js
@@ -3,8 +3,8 @@
 
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-module.exports = buildModule("TokenModule", () => {
-  const token = contract("Token");
+module.exports = buildModule("TokenModule", (m) => {
+  const token = m.contract("Token");
 
   return { token };
 });
